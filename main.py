d6b11643f5bd96602d6f2f8f1d946712a6c6119e
Raju <raju.meenige@gmail.com>
2023-09-05 18:08:36 +0530

    Prepare base model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_o1_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 
 STAGE_NAME='Data Ingestion stage'
@@ -8,6 +10,21 @@ if __name__=='__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj=DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+
+STAGE_NAME='Prepare Base Model'
+
+if __name__=='__main__':
+    try:
+        logger.info(f"*****************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj=PrepareBaseModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
     except Exception as e:
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from cnnClassifier.constants import *
 from cnnClassifier.utils.comman import read_yaml,create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig)
 
 
 class configurationManager:
@@ -26,3 +26,24 @@ class configurationManager:
           )
 
           return data_ingestion_config
+    
+    def get_prepare_base_model_config(self)->PrepareBaseModelConfig:
+        config=self.config.prepare_base_model
+        
+        create_directories(self.root_dir)
+
+        prepare_base_model_config=PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.LEARNING_RATE,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+            
+        )
+        
+        return prepare_base_model_config
+    
+    
+
