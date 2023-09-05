319ddb5fe24cff8cc089c7ff7d9e4f0a4bae90b5
Raju <raju.meenige@gmail.com>
2023-09-05 22:47:33 +0530

    Evaluation Stage

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_o1_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline  
-
+from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 
 STAGE_NAME='Data Ingestion stage'
@@ -40,6 +40,19 @@ if __name__=='__main__':
         logger.info(f"*****************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj=ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME='Evaluation'
+
+if __name__=='__main__':
+    try:
+        logger.info(f"*****************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj=EvaluationPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
     except Exception as e:
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,10 +1,12 @@
 from cnnClassifier.constants import *
 import os
+from pathlib import Path
 from cnnClassifier.utils.comman import read_yaml,create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
                                                 PrepareCallbacksConfig,
-                                                TrainingConfig)
+                                                TrainingConfig,
+                                                EvaluationConfig)
 
 
 class configurationManager:
@@ -49,17 +51,7 @@ class configurationManager:
         
         return prepare_base_model_config
     
-    
-class configurationManager:
-    def __int__(
-            self,
-            config_filepath=CONFIG_FILE_PATH,
-            param_filepath=PARAMS_FILE_PATH):
-            self.config=read_yaml(config_filepath)
-            self.params=read_yaml(param_filepath)
 
-            create_directories([self.config.artifacts_root])
-    
     def get_prepare_callback_config(self)->PrepareCallbacksConfig:
           config=self.config.prepare_callbacks
           model.ckpt_dir=os.path.dirname(config.checkpoint_model_filepath)
@@ -79,16 +71,6 @@ class configurationManager:
           return prepare_callback_config
     
 
-    class configurationManager:
-    def __int__(
-            self,
-            config_filepath=CONFIG_FILE_PATH,
-            param_filepath=PARAMS_FILE_PATH):
-            self.config=read_yaml(config_filepath)
-            self.params=read_yaml(param_filepath)
-
-            create_directories([self.config.artifacts_root])
-    
 
     def get_prepare_callback_config(self)->PrepareCallbacksConfig:
           config=self.config.prepare_callbacks
@@ -131,3 +113,15 @@ class configurationManager:
           )
 
           return training_config
+    
+    def get_validation_config(self)->EvaluationConfig:
+          eval_config=EvaluationConfig(
+                path_of_model=Path("artifacts/training/model.h5"),
+                training_data=Path("artifacts/data_ingestion/Chiken-fecal-images"),
+                all_params=self.params,
+                params_image_size=self.params.IMAGE_SIZE,
+                params_batch_size=self.params.BATCH_SIZE
+          )
+
+          return eval_config
+    
